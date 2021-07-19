40be3a4e375193459a3fabeb6d5ad964276bf839
DarkenVamp <hiteshd1207@gmail.com>
2021-07-19 17:52:54 +0530

    nitd_result: generate an excel file for easier reading

--- nitd_results.py
+++ nitd_results.py
@@ -1,12 +1,17 @@
 import requests
 import json
+import pandas as pd
+from collections import defaultdict
 
 DETAILS_PAGE = "https://erp.nitdelhi.ac.in/CampusLynxNITD/CounsellingRequest?sid=2002&refor=StudentSeatingMasterService"
 RESULT_PAGE = DETAILS_PAGE.replace("2002", "2005")
 SUBJECT_PAGE = DETAILS_PAGE.replace("2002", "2003")
 HEADER = {"Content-Type": "application/x-www-form-urlencoded"}
+BATCHES = {"CSE": 57, "ECE": 56, "EEE": 51}
 jdata = {"sid": "validate", "instituteID": "NITDINSD1506A0000001",
          "mname": "ExamSgpaCgpaDetailOfStudent"}
+df = [defaultdict(list), defaultdict(
+    list), defaultdict(list), defaultdict(list)]
 
 
 def response(url, data):
@@ -17,7 +22,12 @@ def response(url, data):
             continue
 
 
-def result(r_no):
+def add_to_dict(pre, key, val):
+    df[pre][key].append(val)
+    df[0][key].append(val)
+
+
+def result(r_no, pre):
     stud_id = int(r_no[-2:]) + 56*('300' in r_no) + 106*('200' in r_no)
     jdata["studentID"] = 'NITDSTUT2012A0000' + str(stud_id).zfill(3)
     stud_data = 'jdata=' + json.dumps(jdata)
@@ -27,26 +37,29 @@ def result(r_no):
     r_result = response(RESULT_PAGE, stud_data)
 
     if r_result.json():
-        print("Roll No:", r_no)
+        add_to_dict(pre, 'Roll No', int(r_no))
 
         name = r_stud_info.json()[0]['name'].title()
-        print("Name:", name)
+        print("Processing", name)
+        add_to_dict(pre, 'Name', name)
 
         for sem, res in enumerate(r_result.json(), 1):
             jdata["stynumber"] = sem
             sub_data = 'jdata=' + json.dumps(jdata)
             r_sub_info = response(SUBJECT_PAGE, sub_data)
 
-            print("Sem", sem, "Results :-")
             for sub in r_sub_info.json():
-                print(sub["subjectcode"], '-', sub['grade'])
+                df[pre][sub["subjectcode"]].append(sub['grade'])
 
-            print("CGPA :", res['cgpa_r'], '\n')
+        add_to_dict(pre, 'CGPA', res['cgpa_r'])
 
 
-batches = {"CSE": 57, "ECE": 56, "EEE": 51}
-for pre, (batch, strength) in enumerate(batches.items(), 1):
-    print(batch, ":\n")
+for pre, strength in enumerate(BATCHES.values(), 1):
     for i in range(1, strength):
         roll_no = '2012' + str(pre) + '00' + str(i).zfill(2)
-        result(roll_no)
+        result(roll_no, pre)
+
+writer = pd.ExcelWriter('bob.xlsx')
+for pre, sheet in enumerate(['CGPA'] + list(BATCHES)):
+    pd.DataFrame.from_dict(df[pre]).to_excel(writer, sheet, index=None)
+writer.save()
