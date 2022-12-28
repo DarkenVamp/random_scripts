5a3e0da070823824b7da81e2b109535ca5289843
Hitesh Deswal <hiteshd1207@gmail.com>
2022-12-28 20:05:13 +0530

    nitd_results: use multiprocessing
    * disable SSL verification because carnt college peeps don't have valid
      SSL certificate anymore, also silence respective warning
    * use pandas checks for year back or dropouts instead of hardcoding in
      dictionary
    * result() now returns a dictionary for compatibility with
      multiprocessing
    * some minor refactoring

--- nitd_results.py
+++ nitd_results.py
@@ -1,67 +1,80 @@
 import requests
+from urllib3.exceptions import InsecureRequestWarning
 import json
 import pandas as pd
-from collections import defaultdict
+from multiprocessing import Pool
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 DETAILS_PAGE = "https://erp.nitdelhi.ac.in/CampusLynxNITD/CounsellingRequest?sid=2002&refor=StudentSeatingMasterService"
 RESULT_PAGE = DETAILS_PAGE.replace("2002", "2005")
 SUBJECT_PAGE = DETAILS_PAGE.replace("2002", "2003")
 HEADER = {"Content-Type": "application/x-www-form-urlencoded"}
-BATCHES = {
-    "CSE": sorted((set(range(201210001, 201210057)) | {201220039, 201230021, 201230045}) - {201210017, 201210024, 201210044}),
-    "ECE": sorted((set(range(201220001, 201220056)) | {201230039}) - {201220009, 201220014, 201220023, 201220039, 201220040, 201220042, 201220050, 201220051}),
-    "EEE": sorted(set(range(201230001, 201230051)) - {201230007, 201230008, 201230021, 201230023, 201230045, 201230039, 201230041})
+BRANCHES = {
+    "CSE": list(range(201210001, 201210057)) + [201220039, 201230021, 201230045],
+    "ECE": sorted((set(range(201220001, 201220056)) | {201230039}) - {201220039}),
+    "EEE": sorted(set(range(201230001, 201230051)) - {201230021, 201230039, 201230045})
 }
 jdata = {"sid": "validate", "instituteID": "NITDINSD1506A0000001",
          "mname": "ExamSgpaCgpaDetailOfStudent"}
-df = [defaultdict(list), defaultdict(
-    list), defaultdict(list), defaultdict(list)]
 
 
-def response(url, data):
+def response(url: str, data: str) -> requests.Response:
     while 69:
         try:
-            return requests.post(url, headers=HEADER, data=data)
+            return requests.post(url, headers=HEADER, data=data, verify=False)
         except requests.exceptions.ConnectionError:
             continue
 
 
-def add_to_dict(pre, key, val):
-    df[pre][key].append(val)
-    df[0][key].append(val)
-
-
-def result(r_no, pre):
+def result(r_no: str) -> dict:
     stud_id = int(r_no[-2:]) + 56*('300' in r_no) + 106*('200' in r_no)
     jdata["studentID"] = 'NITDSTUT2012A0000' + str(stud_id).zfill(3)
     stud_data = 'jdata=' + json.dumps(jdata)
 
     r_stud_info = response(DETAILS_PAGE, stud_data)
-
     r_result = response(RESULT_PAGE, stud_data)
 
-    if r_result.json():
-        add_to_dict(pre, 'Roll No', int(r_no))
+    if r := r_result.json():
+        if not r[-1]['sgpa_r']:
+            return {}
 
         name = r_stud_info.json()[0]['name'].title()
         print("Processing", name)
-        add_to_dict(pre, 'Name', name)
+        result_info = {'Roll No': int(r_no), 'Name': name}
 
-        jdata["stynumber"] = len(r_result.json())
+        jdata["stynumber"] = len(r)
         sub_data = 'jdata=' + json.dumps(jdata)
         r_sub_info = response(SUBJECT_PAGE, sub_data)
 
         for sub in r_sub_info.json():
-            df[pre][sub["subjectdesc"]].append(sub['grade'])
+            result_info[sub["subjectdesc"]] = sub['grade']
 
-        add_to_dict(pre, 'CGPA', r_result.json()[-1]['cgpa_r'])
-        add_to_dict(pre, 'SGPA', r_result.json()[-1]['sgpa_r'])
+        result_info['CGPA'], result_info["SGPA"] = r[-1]['cgpa_r'], r[-1]['sgpa_r']
+        return result_info
 
 
-for pre, batch_rolls in enumerate(BATCHES.values(), 1):
-    for roll_no in batch_rolls:
-        result(str(roll_no), pre)
+if __name__ == '__main__':
+    df, gpa = [], []
 
-with pd.ExcelWriter('bob.xlsx') as writer:
-    for pre, sheet in enumerate(['GPA'] + list(BATCHES)):
-        pd.DataFrame.from_dict(df[pre]).to_excel(writer, sheet, index=None)
+    with Pool() as pool:
+        for branch_rolls in BRANCHES.values():
+            cur_branch = []
+
+            for res_info in pool.imap(result, map(str, branch_rolls)):
+                if not res_info:
+                    continue
+                gpa_data = list(res_info.items())
+                gpa_data = dict(gpa_data[:2] + gpa_data[-2:])
+                gpa.append(gpa_data)
+                cur_branch.append(res_info)
+            df.append(pd.DataFrame(cur_branch))
+
+    df.insert(0, pd.DataFrame(gpa))
+
+    # Remove backlog/dropouts by checking if they do not have the first subject of first roll number
+    for i in range(4):
+        df[i] = df[i].dropna(subset=[df[i].columns[2]]).dropna(axis=1)
+
+    with pd.ExcelWriter('bob.xlsx') as writer:
+        for i, sheet in enumerate(['GPA'] + list(BRANCHES)):
+            df[i].to_excel(writer, sheet, index=None)
