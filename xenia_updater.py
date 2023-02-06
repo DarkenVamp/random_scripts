e82b18e6d59485bc944602a8b8475a66f9148dae
DarkenVamp <hiteshd1207@gmail.com>
2023-02-06 18:04:39 +0530

    xenia_updater: a basic updater for xenia
    It compares timestamps to determine if existing version is older.
    Can be used with --build argument, uses canary channel by default.
    Run it in the same directory as xenia executable.

---
+++ xenia_updater.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup as Soup
+import os
+from dateutil import parser
+from zipfile import ZipFile
+from argparse import ArgumentParser
+
+CANARY_URL = 'https://github.com/xenia-canary/xenia-canary/releases/latest/'
+CANARY_DOWNLOAD = CANARY_URL + 'download/xenia_canary.zip'
+MASTER_URL = 'https://github.com/xenia-project/release-builds-windows/releases/latest'
+MASTER_DOWNLOAD = MASTER_URL + 'download/xenia_master.zip'
+
+
+class XeniaBuild:
+    def __init__(self, release_page: str, download_url: str, executable: str) -> None:
+        self.release_page = release_page
+        self.download_url = download_url
+        self.executable = executable
+        self.zip_name = self.download_url.split('/')[-1]
+
+
+BUILD_TYPES = {
+    'canary': XeniaBuild(CANARY_URL, CANARY_DOWNLOAD, 'xenia_canary.exe'),
+    'master': XeniaBuild(MASTER_URL, MASTER_DOWNLOAD, 'xenia.exe')
+}
+
+
+def is_newer_build_available() -> bool:
+    print("Checking for newer builds...")
+
+    # fetch timestamp of latest release
+    r = requests.get(BUILD.release_page)
+    soup = Soup(r.content, 'lxml')
+    time = soup.find('relative-time')['datetime']
+    timestamp = parser.isoparse(time).timestamp()
+
+    # check timestamp of existing exe or download if exe is not available
+    try:
+        cur_timestamp = os.path.getmtime(BUILD.executable)
+    except FileNotFoundError:
+        return True
+
+    return timestamp > cur_timestamp
+
+
+def download_build() -> None:
+    print("Downloading latest build...")
+
+    # write zip contents as binary
+    r = requests.get(BUILD.download_url)
+    with open(BUILD.zip_name, 'wb') as f:
+        f.write(r.content)
+
+
+def unzip() -> None:
+    print(f"Unzipping {BUILD.zip_name}...")
+
+    # extract all files into current directory and delete zip
+    with ZipFile(BUILD.zip_name, 'r') as zf:
+        zf.extractall('.')
+    os.remove(BUILD.zip_name)
+
+
+if __name__ == '__main__':
+    # usage: xenia_updater.py --build {master, canary}
+    # takes 'canary' by default
+    argparser = ArgumentParser(prog="xenia_updater.py")
+    argparser.add_argument(
+        '--build',
+        choices=['master', 'canary'],
+        default='canary'
+    )
+    args = argparser.parse_args()
+
+    BUILD = BUILD_TYPES.get(vars(args)['build'])
+
+    if is_newer_build_available():
+        download_build()
+        unzip()
+    else:
+        print("Already up to date...")
